596630b8638059146751fef0abdb3ada743a71e0
Storm <87811650+Storm99999@users.noreply.github.com>
2021-07-22 18:40:55 +0200

    Add files via upload

---
+++ DiscordTools/Config.cs
@@ -0,0 +1,26 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiscordTools
+{
+    public static class Config
+    {
+
+
+        public static void A()
+        {
+            File.Create("name.conf");
+            File.Create("webhook.conf");
+            File.Create("content.conf");
+        }
+
+        public static void yes()
+        {
+        }
+    }
+}
---
+++ DiscordTools/Http.cs
@@ -0,0 +1,8 @@
+using System;
+
+namespace DiscordTools
+{
+    class Http {
+        
+    }
+}
